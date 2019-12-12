f90405bddaea1bb1f756ea50ffc5bce9d9d26a72
Kartikay Singh <kartikays89@gmail.com>
2019-12-12 11:14:07 +0100

    Updates

---
+++ emailCounters.py
@@ -0,0 +1,54 @@
+"""
+
+Let's suppose we want to analyse the email addresses which participants of this challenge 
+used for registration. Write a function called check_domains(list_of_emails) that takes as 
+input a list of email addresses (strings) and returns a Counter (if you don't know what a 
+Counter is, please go to the resources first) showing the number of email addresses by 
+domain (the part that comes after '@').
+
+"""
+import re
+from collections import Counter
+
+listOfEmails = ['abc@gmail.com', 'abc@yahoo.com', 'abc@yahoo.nl', 'abc@yahoo.co.in', 'xyz@gmail.com',
+'xyz@yahoo.in', 'xyz@yahoo.nl', 'xyz@yahoo.com', 'xyz2@gmail.com', 'kar@yahoo.com', 'xyz@yahoo.co.in, xyz@gmail.com']
+
+
+# Using the counter module
+
+def check_domains(list_of_emails):
+	gmailCounter = 0
+	yahooNLCounter = 0
+	yahooINCounter = 0
+	yahooComCounter = 0
+
+	gmailRegex = r'\bgmail\b'
+	yahooNL = r'\bnl\b'
+	yahooIN = r'\bin\b'
+	yahooCom = r'\byahoo.com\b'
+	
+
+	for emails in list_of_emails:
+		gmailX = re.findall(gmailRegex, emails)
+		if gmailX:
+			gmailCounter +=1
+
+		yahoonlX = re.findall(yahooNL, emails)
+		if yahoonlX:
+			yahooNLCounter +=1
+
+		yahooInX = re.findall(yahooIN, emails)
+		if yahooInX:
+			yahooINCounter +=1
+
+		yahooCOmX = re.findall(yahooCom, emails)
+		if yahooCOmX:
+			yahooComCounter +=1
+
+	
+	return 'Gmail addresses are: {}, Netherlands yahoo are: {}, Indian Yahoo are: {}, Yahoo.com are: {} '.format(gmailCounter,
+		yahooNLCounter, yahooINCounter, yahooComCounter)
+	
+
+print(check_domains(listOfEmails))
+
--- intersection.py
+++ intersection.py
@@ -34,3 +34,20 @@ def fun_with_sets(set1, set2):
 
 
 print(fun_with_sets(mailNotInsta, instaNotMail))
+
+"""
+
+# beginner
+def fun_with_sets(set1, set2):
+  return len(set1.intersection(set2))
+
+testyourcode.check_funcion(fun_with_sets)
+
+# advanced
+def fun_with_sets2(set1, set2):
+  return len((set1.union(set2)).difference(set1.symmetric_difference(set2)))
+
+testyourcode.check_funcion(fun_with_sets2)
+
+
+"""
